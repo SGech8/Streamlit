b24a252f939a3b021073b319e7aca5c5ea94a6c2
SGech8 <jan-niklas.zutt@stud.uni-due.de>
2021-12-02 20:29:25 +0100

    test

--- main.py
+++ main.py
@@ -74,6 +74,7 @@ def einlesen(files):
 
 
 if st.session_state.start == 1:
+    st.header("Test für push")
     # TODO: hier noch anpasen wegen Anzahl der Dateien!
     numberOfFiles = len(anzahlEssays())
     #st.write(file)
